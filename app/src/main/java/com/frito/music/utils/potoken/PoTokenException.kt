35f66dde3df24063a8ba650e4405f1042b4aa54a
kurtcobain2728 <kurtcobain272829@gmail.com>
2026-07-13 10:45:34 -0400

    feat: add PoToken data classes

---
+++ app/src/main/java/com/frito/music/utils/potoken/PoTokenException.kt
@@ -0,0 +1,4 @@
+package com.frito.music.utils.potoken
+
+class PoTokenException(message: String) : Exception(message)
+class BadWebViewException(message: String) : Exception(message)
---
+++ app/src/main/java/com/frito/music/utils/potoken/PoTokenResult.kt
@@ -0,0 +1,6 @@
+package com.frito.music.utils.potoken
+
+class PoTokenResult(
+    val playerRequestPoToken: String,
+    val streamingDataPoToken: String,
+)
